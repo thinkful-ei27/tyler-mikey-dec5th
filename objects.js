2292d804649541ce7df3c13c943a44207365bac9
Tyler Crabb <tydacrabb@gmail.com>
2018-12-05 14:39:16 -0800

    started database search

--- objects.js
+++ objects.js
@@ -126,9 +126,35 @@ characters.push(new CreateCharacter('Arwen Undomiel', 'Arwen', 'Half-Elf', 'Rive
 // console.log(characters);
 
 const aragornDescribe = characters.find(character => { if 
- (character.nickname === 'Aragorn'){
-   return character.describe();
- }
+(character.nickname === 'Aragorn'){
+  return character.describe();
+}
 });
 
-console.log(aragornDescribe)
+// console.log(aragornDescribe)
+
+const hobbits = characters.filter(character => character.race === 'Hobbit');
+console.log(hobbits);
+
+const strongCharacters = characters.filter(character => character.attack > 5);
+console.log(strongCharacters);
+
+
+const HEROES = [
+  { id: 1, name: 'Captain America', squad: 'Avengers' },
+  { id: 2, name: 'Iron Man', squad: 'Avengers' },
+  { id: 3, name: 'Spiderman', squad: 'Avengers' },
+  { id: 4, name: 'Superman', squad: 'Justice League' },
+  { id: 5, name: 'Wonder Woman', squad: 'Justice League' },
+  { id: 6, name: 'Aquaman', squad: 'Justice League' },
+  { id: 7, name: 'Hulk', squad: 'Avengers' },
+];
+
+function findOne(arr, query){
+  let keys = Object.keys(query);
+  
+
+
+
+
+
